5d2b4c57cb8f001ce80095a6e5543541d386bb9d
Aniket Shinde <aniket@dvnks.com>
2026-02-26 11:39:09 +0000

    added job screeining doctypes

---
+++ supremusangel/supremus_angel/doctype/screening_questions/screening_questions.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Aniket Shinde and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Screening Questions", {
+// 	refresh(frm) {
+
+// 	},
+// });
